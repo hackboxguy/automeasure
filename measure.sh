835907b7b32b42460e4831bf27557e9e4b4a435e
hackboxguy <albert.david@gmail.com>
2024-10-21 16:13:38 +0200

    added rgbw measurements befor and after loop-test

--- measure.sh
+++ measure.sh
@@ -2,9 +2,6 @@
 DATE=$(date "+%Y%m%d-%H%M%S")
 export LD_LIBRARY_PATH=/home/pi/automeasure/brbox/output/lib
 
-#RGBW measurement
-#/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=8000 --interval=10 --tempered=tempered --wfile=white.png --rfile=red.png --gfile=green.png --bfile=blue.png > /home/pi/automeasure/$DATE.csv
-
 #check if ka3005p power supply is available
 /home/pi/automeasure/binaries/ka3005p status > /dev/null
 if [ $? = 0 ]; then
@@ -20,7 +17,17 @@ else
 	TEMPARG=""
 fi
 
-#white measurement(keep the pattern fixed
-/home/pi/automeasure/brbox/output/bin/mplayclt --showimg=none --showimg=/home/pi/automeasure/patterns/white.png
-#/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=8000 --interval=20 --power=ka3005p --tempered=tempered --wfile=white.png --startupimg=white.png --measureonly=yes > /home/pi/automeasure/$DATE.csv
-/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=8000 --interval=20 $PSARG $TEMPARG --wfile=white.png --startupimg=white.png --measureonly=yes > /home/pi/automeasure/$DATE.csv
+#1-initial triangle
+/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=3 --interval=5 $PSARG $TEMPARG --wfile=white.png --rfile=red.png --gfile=green.png --bfile=blue.png > /home/pi/automeasure/rgbw-begin.csv
+
+#2-white measurement(keep the pattern fixed
+/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=10 --interval=20 $PSARG $TEMPARG --wfile=white.png --startupimg=white.png --measureonly=yes > /home/pi/automeasure/$DATE.csv
+
+#3-initial triangle
+/home/pi/automeasure/measure-color.sh --mypath=/home/pi/automeasure --loop=3 --interval=5 $PSARG $TEMPARG --wfile=white.png --rfile=red.png --gfile=green.png --bfile=blue.png > /home/pi/automeasure/rgbw-end.csv
+
+#4-at the end of the measurement, let the display cooldown
+/home/pi/automeasure/brbox/output/bin/mplayclt --showimg=none
+
+
+#/home/pi/automeasure/brbox/output/bin/mplayclt --showimg=none --showimg=/home/pi/automeasure/patterns/white.png
